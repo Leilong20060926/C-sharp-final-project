62384bd86a2032437f4630a03b72eea159aa7e85
Leilong20060926 <leilong925@gmail.com>
2026-04-21 23:33:42 +0800

    add

---
+++ final project/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace final_project
+{
+    internal class test
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hello from Test!");
+        }
+    }
+}
