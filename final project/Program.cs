a4c0c56467149758ae633299dc009d0ebf418814
Leilong20060926 <leilong925@gmail.com>
2026-04-28 22:12:55 +0800

    add

--- final project/Program.cs
+++ final project/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
-class Program
+namespace final_project
 {
-    static void Main()
+    internal static class Program
     {
-        final_project.test s = new final_project.test();
-        s.SayHi();
-        final_project.Zz h = new final_project.Zz();
-        h.Zzz();
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
     }
 }
