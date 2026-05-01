a41641a075043d5b567d6ce44733c52e3591d30b
yannneedsmoney <159357456mimi@gmail.com>
2026-05-01 17:00:38 +0800

    add

---
+++ WinFormsApp2/FormA.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp2
+{
+    public partial class FormA : Form
+    {
+        public FormA()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
